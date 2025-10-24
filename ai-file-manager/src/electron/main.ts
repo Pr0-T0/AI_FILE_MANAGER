b6a5a008c5a36ad62efa240ffdea6803e5955b85
Pr0-T0 <sarathsasidharan557@gmail.com>
2025-10-24 05:45:08 +0530

    removed menubar

--- ai-file-manager/src/electron/main.ts
+++ ai-file-manager/src/electron/main.ts
@@ -1,10 +1,13 @@
-import {app, BrowserWindow} from "electron"
+import {app, BrowserWindow, Menu} from "electron"
 import path from "path";
 import { isDev } from "./util.js";
 
 
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({});
+
+    Menu.setApplicationMenu(null);
+
     if (isDev()) {
         mainWindow.loadURL('http://localhost:5432');
     }else{
